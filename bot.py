2f53aac714875394066292cbb6838207e4f84616
Sebastian Calo <smilingcalo@gmail.com>
2026-04-22 22:24:55 -0400

    Wrote SetupRoles command

--- bot.py
+++ bot.py
@@ -3,6 +3,6 @@ from discord.ext import commands
 from config import *
 
 
-bot = commands.Bot(command_prefix='smfb', descsription=BOT_DESCRIPTION, intents=discord.Intents.default())
+bot = commands.Bot(command_prefix='smfb', description=BOT_DESCRIPTION, intents=discord.Intents(message_content=True))
 
 bot.run(TOKEN)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import discord
+import data
+import discord.ext.commands
 from bot import bot
 '''
 	Commands
@@ -17,17 +18,42 @@ from bot import bot
 		Keep track of last time user applied specific role and role group
 		Allow cooldown for each role and role group that prevents user from re-adding that role
 '''
-
+channels: dict[int, list[int]] = {}
 
 @bot.event
 async def on_ready():
 	print("Bot is online")
 
 @bot.command(name='setup roles', description='Sets up role assignment message in channel')
-async def SetupRoles(ctx):
+async def SetupRoles(ctx: discord.ext.commands.Context):
+	# save channel command was called in
+	channels[ctx.channel.id] = []
+	await ctx.send('React with an emoji below to receive your desired role. Remove your reaction to remove the role!') # Send instructions
+
+	roleGroups = data.GetRoles() 
+	
+	# For each group, build and send the related message
+	for (groupName, roles) in roleGroups.items():
+		# Build message
+		msgText = f'**{groupName}**'
+		for role in roles:
+			msgText += f'\n{role.Emoji} {role.Name}: {role.Description}'
+		
+		# Send message
+		msg = await ctx.send(msgText)
+
+		# Save message id for tracking
+		channels[ctx.channel.id].append(msg.id) 
+
+		# Add reactions to message
+		for role in roles:
+			await ctx.message.add_reaction(role.Emoji)
+
+@bot.event
+async def on_reaction_add(reaction, user):
 	return
 
-
-
-
+@bot.event
+async def on_reaction_remove(reaction, user):
+	return
 
